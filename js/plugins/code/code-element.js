2742f9307d1534ef24d2f79a24c737c68e1c7f31
sohzm <sohambharambe9@gmail.com>
2025-01-27 01:46:52 +0530

    fixed code block

--- js/plugins/code/code-element.js
+++ js/plugins/code/code-element.js
@@ -1,127 +1,206 @@
-class CodeElement extends HTMLElement {
-   constructor() {
-       super();
-       this.attachShadow({ mode: 'open' });
-       this.render();
-       this.valueBuffer = null;
-       this.supportedLanguages = {
-           'javascript': 'JavaScript',
-           'python': 'Python', 
-           'typescript': 'TypeScript',
-           'java': 'Java',
-           'go': 'Go',
-           'cpp': 'C++',
-           'csharp': 'C#',
-           'php': 'PHP',
-           'ruby': 'Ruby',
-           'swift': 'Swift',
-           'kotlin': 'Kotlin',
-           'sql': 'SQL',
-           'html': 'HTML',
-           'css': 'CSS',
-           'markdown': 'Markdown'
-       };
-   }
+import { html, css, LitElement } from '/a7/cdn/lit-core-2.7.4.min.js';
 
-   async connectedCallback() {
-       await this.initializeCodeMirror();
-       this.initializeLanguageSelector();
-   }
+class CodeElement extends LitElement {
+    static styles = css`
+        :host {
+            display: block;
+            position: relative;
+        }
+        select {
+            font-family: var(--font-mono);
+            padding: var(--padding-w1);
+            border: 1px solid var(--border-1);
+            border-radius: var(--radius);
+            background: var(--bg-2);
+            color: var(--text-1);
+            cursor: pointer;
+            position: absolute;
+            top: var(--padding-2);
+            right: var(--padding-2);
+            z-index: 1;
+            outline: none;
+            opacity: 0;
+            transition: opacity 0.2s;
+        }
+        select:focus {
+            outline: none;
+            border-color: var(--accent-text);
+        }
+        :host(:hover) select {
+            opacity: 1;
+        }
+        @media (max-width: 768px) {
+            select {
+                opacity: 1;
+            }
+        }
+        .CodeMirror {
+            height: auto;
+            font-family: var(--font-mono);
+            background: var(--bg-1);
+            color: var(--text-1);
+            border: 1px solid var(--border-1);
+            border-radius: var(--radius);
+            padding: var(--padding-3);
+            caret-color: var(--text-1);
+            font-size: 14px;
+        }
+        .cm-matchingbracket {
+            background-color: var(--bg-green);
+            color: var(--fg-green) !important;
+        }
+        .cm-nonmatchingbracket {
+            background-color: var(--bg-red);
+            color: var(--fg-red) !important;
+        }
+        .CodeMirror-selected {
+            background-color: var(--accent-bg) !important;
+        }
+        .cm-variable { color: var(--text-1); }
+        .cm-keyword { color: var(--fg-purple); }
+        .cm-def { color: var(--fg-blue); }
+        .cm-operator { color: var(--fg-red); }
+        .cm-number { color: var(--fg-orange); }
+        .cm-string { color: var(--accent-text); }
+        .cm-property { color: var(--fg-cyan); }
+        .cm-comment { color: var(--text-2); }
+        .CodeMirror-gutters { display: none; }
+        .CodeMirror-cursor { border-left: 1px solid var(--text-1); }
+    `;
 
-   async initializeCodeMirror() {
-       await import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/codemirror.min.js');
-       
-       const modes = [
-           'javascript', 'xml', 'css', 'python', 'clike', 'markdown',
-           'go', 'sql', 'php', 'ruby', 'swift'
-       ];
-       
-       await Promise.all(modes.map(mode => 
-           import(`https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/mode/${mode}/${mode}.min.js`)
-       ));
+    static properties = {
+        supportedLanguages: { type: Object },
+        editor: { type: Object },
+        valueBuffer: { type: Object },
+        selectedLang: { type: String }
+    };
 
-       await Promise.all([
-           import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/edit/closebrackets.min.js'),
-           import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/edit/matchbrackets.min.js'),
-           import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/edit/closetag.min.js'),
-           import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/comment/comment.min.js'),
-           import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/fold/foldcode.min.js'),
-           import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/fold/brace-fold.min.js')
-       ]);
+    constructor() {
+        super();
+        this.supportedLanguages = {
+            'javascript': 'JavaScript',
+            'python': 'Python', 
+            'typescript': 'TypeScript',
+            'java': 'Java',
+            'go': 'Go',
+            'cpp': 'C++',
+            'csharp': 'C#',
+            'php': 'PHP',
+            'ruby': 'Ruby',
+            'swift': 'Swift',
+            'kotlin': 'Kotlin',
+            'sql': 'SQL',
+            'html': 'HTML',
+            'css': 'CSS',
+            'markdown': 'Markdown'
+        };
+        this.valueBuffer = null;
+        this.selectedLang = 'javascript';
+    }
 
-       const editorContainer = this.shadowRoot.querySelector('#editor');
-       
-       this.editor = CodeMirror(editorContainer, {
-           lineNumbers: false,
-           theme: 'custom',
-           mode: 'javascript',
-           lineWrapping: true,
-           indentUnit: 4,
-           tabSize: 4,
-           scrollbarStyle: null,
-           viewportMargin: Infinity,
-           autoCloseBrackets: true,
-           matchBrackets: true,
-           autoCloseTags: true,
-           foldGutter: true,
-           gutters: ["CodeMirror-foldgutter"],
-           extraKeys: {
-               "Tab": (cm) => cm.execCommand("indentMore"),
-               "Shift-Tab": (cm) => cm.execCommand("indentLess"),
-               "Ctrl-/": "toggleComment",
-               "Cmd-/": "toggleComment",
-               "Ctrl-J": "toMatchingTag",
-               "Ctrl-Space": "autocomplete"
-           }
-       });
+    async firstUpdated() {
+        await this.initializeCodeMirror();
+        this.initializeLanguageSelector();
+    }
 
-       this.editor.on('change', () => {
-           this.sendUpdates();
-       });
+    async initializeCodeMirror() {
+        await import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/codemirror.min.js');
+        
+        const modes = [
+            'javascript', 'xml', 'css', 'python', 'clike', 'markdown',
+            'go', 'sql', 'php', 'ruby', 'swift'
+        ];
+        
+        await Promise.all([
+            ...modes.map(mode => 
+                import(`https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/mode/${mode}/${mode}.min.js`)
+            ),
+            import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/edit/closebrackets.min.js'),
+            import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/edit/matchbrackets.min.js'),
+            import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/edit/closetag.min.js'),
+            import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/comment/comment.min.js'),
+            import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/fold/foldcode.min.js'),
+            import('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/addon/fold/brace-fold.min.js')
+        ]);
 
-       if (this.valueBuffer) {
-           this.setValue(null, this.valueBuffer);
-           this.valueBuffer = null;
-       }
-   }
+        const editorContainer = this.renderRoot.querySelector('#editor');
+        
+        this.editor = CodeMirror(editorContainer, {
+            lineNumbers: false,
+            theme: 'custom',
+            mode: this.getModeForLanguage(this.selectedLang),
+            lineWrapping: true,
+            indentUnit: 4,
+            tabSize: 4,
+            scrollbarStyle: null,
+            viewportMargin: Infinity,
+            autoCloseBrackets: true,
+            matchBrackets: true,
+            autoCloseTags: true,
+            foldGutter: true,
+            gutters: ["CodeMirror-foldgutter"],
+            extraKeys: {
+                "Tab": (cm) => cm.execCommand("indentMore"),
+                "Shift-Tab": (cm) => cm.execCommand("indentLess"),
+                "Ctrl-/": "toggleComment",
+                "Cmd-/": "toggleComment",
+                "Ctrl-J": "toMatchingTag",
+                "Ctrl-Space": "autocomplete"
+            }
+        });
 
-   initializeLanguageSelector() {
-       const select = this.shadowRoot.querySelector('#language-select');
-       select.value = 'javascript';
-       Object.entries(this.supportedLanguages).forEach(([value, label]) => {
-           const option = document.createElement('option');
-           option.value = value;
-           option.textContent = label;
-           select.appendChild(option);
-       });
-       
-       select.addEventListener('change', (e) => {
-           const mode = this.getModeForLanguage(e.target.value);
-           this.editor.setOption('mode', mode);
-           this.sendUpdates();
-       });
-   }
+        this.editor.on('change', () => {
+            this.sendUpdates();
+        });
 
-   getModeForLanguage(lang) {
-       const modeMap = {
-           javascript: 'javascript',
-           typescript: 'javascript',
-           java: 'text/x-java',
-           cpp: 'text/x-c++src',
-           csharp: 'text/x-csharp',
-           python: 'python',
-           go: 'go',
-           php: 'php',
-           ruby: 'ruby',
-           swift: 'swift',
-           kotlin: 'text/x-kotlin',
-           sql: 'sql',
-           html: 'xml',
-           css: 'css',
-           markdown: 'markdown'
-       };
-       return modeMap[lang] || lang;
-   }
+        if (this.valueBuffer) {
+            this.setValue(null, this.valueBuffer);
+            this.valueBuffer = null;
+        }
+    }
+
+    initializeLanguageSelector() {
+        const select = this.renderRoot.querySelector('#language-select');
+        select.value = this.selectedLang;
+        select.addEventListener('change', (e) => {
+            this.selectedLang = e.target.value;
+            const mode = this.getModeForLanguage(this.selectedLang);
+            this.editor.setOption('mode', mode);
+            this.sendUpdates();
+        });
+    }
+
+    updated(changedProperties) {
+        if (changedProperties.has('selectedLang') && this.editor) {
+            const mode = this.getModeForLanguage(this.selectedLang);
+            this.editor.setOption('mode', mode);
+            const select = this.renderRoot.querySelector('#language-select');
+            if (select && select.value !== this.selectedLang) {
+                select.value = this.selectedLang;
+            }
+        }
+    }
+
+    getModeForLanguage(lang) {
+        const modeMap = {
+            javascript: 'javascript',
+            typescript: 'javascript',
+            java: 'text/x-java',
+            cpp: 'text/x-c++src',
+            csharp: 'text/x-csharp',
+            python: 'python',
+            go: 'go',
+            php: 'php',
+            ruby: 'ruby',
+            swift: 'swift',
+            kotlin: 'text/x-kotlin',
+            sql: 'sql',
+            html: 'xml',
+            css: 'css',
+            markdown: 'markdown'
+        };
+        return modeMap[lang] || lang;
+    }
 
     setValue(path, value) {
         if (!this.editor) {
@@ -130,130 +209,51 @@ class CodeElement extends HTMLElement {
         }
         const content = value.textContent || '';
         this.editor.setValue(content);
-        const mode = this.getModeForLanguage(value.language);
-        this.editor.setOption('mode', mode);
-        this.shadowRoot.querySelector('#language-select').value = value.language;
+        this.selectedLang = value.language || 'javascript';
     }
 
-   getValue() {
-       if (!this.editor) return this.valueBuffer;
-       return {
-           textContent: this.editor.getValue(),
-           language: this.shadowRoot.querySelector('#language-select').value
-       };
-   }
+    getValue() {
+        if (!this.editor) return this.valueBuffer;
+        return {
+            textContent: this.editor.getValue(),
+            language: this.selectedLang
+        };
+    }
 
-   getTextContent() {
-       if (!this.editor) return { html: '', text: '', markdown: '' };
-       const code = this.editor.getValue();
-       const lang = this.shadowRoot.querySelector('#language-select').value;
-       
-       return {
-           html: `<pre><code class="language-${lang}">${code}</code></pre>`,
-           text: code,
-           markdown: '```' + lang + '\n' + code + '\n```'
-       };
-   }
+    getTextContent() {
+        if (!this.editor) return { html: '', text: '', markdown: '' };
+        const code = this.editor.getValue();
+        
+        return {
+            html: `<pre><code class="language-${this.selectedLang}">${code}</code></pre>`,
+            text: code,
+            markdown: '```' + this.selectedLang + '\n' + code + '\n```'
+        };
+    }
 
-   focus() {
-       if (this.editor) {
-           this.editor.focus();
-       }
-   }
+    focus() {
+        if (this.editor) {
+            this.editor.focus();
+        }
+    }
 
-   sendUpdates() {
-       setTimeout(() => {
-           window.wisk?.editor?.justUpdates(this.id);
-       }, 0);
-   }
+    sendUpdates() {
+        setTimeout(() => {
+            window.wisk?.editor?.justUpdates(this.id);
+        }, 0);
+    }
 
-   render() {
-       const style = `
-           <style>
-           @import url('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/codemirror.min.css');
-           
-           :host {
-               display: block;
-               position: relative;
-           }
-           
-           select {
-               font-family: var(--font-mono);
-               padding: var(--padding-w1);
-               border: 1px solid var(--border-1);
-               border-radius: var(--radius);
-               background: var(--bg-2);
-               color: var(--text-1);
-               cursor: pointer;
-               position: absolute;
-               top: var(--padding-2);
-               right: var(--padding-2);
-               z-index: 1;
-               outline: none;
-           }
-           
-           select:focus {
-               outline: none;
-               border-color: var(--accent-text);
-           }
-           
-           .CodeMirror {
-               height: auto;
-               font-family: var(--font-mono);
-               background: var(--bg-1);
-               color: var(--text-1);
-               border: 1px solid var(--border-1);
-               border-radius: 0 0 var(--radius) var(--radius);
-               padding: var(--padding-2);
-               caret-color: var(--text-1);
-           }
-           
-           .cm-matchingbracket {
-               background-color: var(--bg-green);
-               color: var(--fg-green) !important;
-           }
-           
-           .cm-nonmatchingbracket {
-               background-color: var(--bg-red);
-               color: var(--fg-red) !important;
-           }
-           
-           .CodeMirror-selected {
-               background-color: var(--accent-bg) !important;
-           }
-           
-           .cm-keyword { color: var(--fg-purple); }
-           .cm-def { color: var(--fg-blue); }
-           .cm-variable { color: var(--text-1); }
-           .cm-operator { color: var(--fg-red); }
-           .cm-number { color: var(--fg-orange); }
-           .cm-string { color: var(--fg-green); }
-           .cm-comment { color: var(--text-2); }
-           .cm-property { color: var(--fg-cyan); }
-           
-           ::-webkit-scrollbar { width: 15px; }
-           ::-webkit-scrollbar-track { background: var(--bg-1); }
-           ::-webkit-scrollbar-thumb { 
-               background-color: var(--bg-3);
-               border-radius: 20px;
-               border: 4px solid var(--bg-1);
-           }
-           ::-webkit-scrollbar-thumb:hover {
-               background-color: var(--text-1);
-           }
-           .CodeMirror-gutters {
-               display: none;
-           }
-            .CodeMirror-cursor {
-                border-left: 1px solid var(--text-1);
-            }
-           </style>
-           <select id="language-select"></select>
-           <div id="editor"></div>
-       `;
-       
-       this.shadowRoot.innerHTML = style;
-   }
+    render() {
+        return html`
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.2/codemirror.min.css">
+            <select id="language-select">
+                ${Object.entries(this.supportedLanguages).map(([value, label]) => 
+                    html`<option value="${value}" ?selected=${value === this.selectedLang}>${label}</option>`
+                )}
+            </select>
+            <div id="editor"></div>
+        `;
+    }
 }
 
 customElements.define('code-element', CodeElement);
